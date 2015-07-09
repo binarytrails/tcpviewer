76ef9bd03ff2e5f256f33ed8ec323ffaa6f00d8e
sevaivanov <sevaivanov@kedfilms.com>
2015-07-09 01:03:28 -0400

    + script help with address args

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,13 +1,28 @@
 var fs = require('fs'),
-    express = require('express');
+    program = require('commander'),
+    ip_port_regex = require('ip-port-regex'),
+    express = require('express'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     _ = require("underscore"),
     path = require('path');
 
-server.listen(8080);
+program
+  .version('0.1.0')
+  .usage('[options] <file ...>')
+  .option('-a, --address <ip:port>', 'listen on the ip address and port', ip_port_regex.v4())
+  .parse(process.argv);
 
+server.listen(ip_port_regex.parts(program.address)['port'],
+        ip_port_regex.parts(program.address)['ip'],
+        function(){
+            console.log("Listening on http://%s:%s",
+                    server.address().address,
+                    server.address().port
+            );
+        }
+);
 
 var WatchIO = require('watch.io'),
     watcher = new WatchIO({
@@ -31,7 +46,7 @@ io.on('connection', function (socket) {
     socket.emit('images', images);
 
     watcher.on('change', function ( type, file, stat ) {
-        console.log(file);
+        //console.log(file);
         socket.emit('image', file.replace(/^.*[\\\/]/, ''));
     });
 
