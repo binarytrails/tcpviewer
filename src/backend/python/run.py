a36b6579129d4d70124025fbfd2d46f222875ba8
sevaivanov <sevaivanov@kedfilms.com>
2015-07-30 12:06:24 -0400

    - getopt, + docopt

--- src/backend/python/run.py
+++ src/backend/python/run.py
@@ -1,53 +1,21 @@
 #!/usr/bin/python2.7
 
-"""
-    An entry point to run the backend.
+"""Tcpflow Visualiser.
+
+Usage:
+    tcp_visualiser.py [-v] (-i) <interface> [-c] (-o) <directory>
+
+Opitons:
+    -v, --verbose       Show relevant print's during program execution.
+    -i, --interface     Listen on the network interface.
+    -c, --clean         Clean output directory.
+    -o, --output        Output directory.
 """
 
-import sys, getopt
-
+from docopt import docopt
 from Main import Main
 
-def main(argv):
-    interface = None
-    output = None
-    clean = False
-    verbose = True
-
-    help_message = 'run.py -v -i <interface> -o <output> --clean-existing'
-
-    try:
-        opts, args = getopt.getopt(argv,'hvi:o:c', ['clean-existing'])
-
-    except getopt.GetoptError as error:
-        print error
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print help_message
-
-        elif opt in ('-v'):
-            verbose = True
-
-        elif opt in ('-i'):
-            if str(arg) == "list":
-                pass #TODO
-            else:
-                interface = arg
-
-        elif opt in ('-o'):
-            output = arg
-
-        elif opt in ('-c', '--clean-existing'):
-            clean = True
-
-    if (output or interface) == None:
-        print help_message
-        sys.exit()
-
-    Main(interface, output, clean, verbose)
-
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    args = docopt(__doc__, version='Tcpflow Visualiser 0.1')
+    Main(args["<interface>"], args["<directory>"], args["--clean"], args["--verbose"])
 
